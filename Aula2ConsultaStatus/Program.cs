527d2fabd9955720410da6a68beae8725af0acee
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-10 00:34:42 -0300

    finalizado aula 2

--- Aula2ConsultaStatus/Program.cs
+++ Aula2ConsultaStatus/Program.cs
@@ -6,6 +6,7 @@ using DFe.Utils;
 using NFe.Classes.Informacoes.Identificacao.Tipos;
 using NFe.Servicos;
 using NFe.Utils;
+using UtilsDFeNFe.NFeNfceExtensions;
 
 namespace Aula2ConsultaStatus
 {
@@ -23,11 +24,11 @@ namespace Aula2ConsultaStatus
                     CacheId = "b2543ace-2708-4660-8ea8-89ca845d10bf",
                     ManterDadosEmCache = true,
                     //Senha = "minhasenha do certificado digital",
-                    Serial = "65E7A12445262E37"
+                    Serial = "7CF486CADF6F2C54"
                 },
                 DefineVersaoServicosAutomaticamente = true,
                 VersaoLayout = VersaoServico.Versao400,
-                cUF = Estado.GO,
+                cUF = Estado.MT,
                 DiretorioSalvarXml = @"C:\MeuSistema\Xmls",
                 SalvarXmlServicos = true,
                 DiretorioSchemas = @"C:\MeuSistema\Schemas",
@@ -43,6 +44,8 @@ namespace Aula2ConsultaStatus
             var servicos = new ServicosNFe(configuracaoServico);
 
             var retorno = servicos.NfeStatusServico();
+
+            var retornoStatusSucesso = retorno.ServicoEmOperacao();
 
             Console.WriteLine(retorno.EnvioStr);
         }
---
+++ UtilsDFeNFe/NFeNfceExtensions/RetornoNFeStatusServicoExtension.cs
@@ -0,0 +1,35 @@
+﻿using NFe.Servicos.Retorno;
+
+namespace UtilsDFeNFe.NFeNfceExtensions
+{
+    public static class RetornoNFeStatusServicoExtension
+    {
+        public static bool ServicoEmOperacao(this RetornoNfeStatusServico retornoNfeStatusServico)
+        {
+            if (retornoNfeStatusServico == null) return false;
+
+            if (retornoNfeStatusServico.Retorno.cStat == 107) return true;
+
+            return false;
+        }
+
+        public static bool ParalisadoTemporariamente(this RetornoNfeStatusServico retornoNfeStatusServico)
+        {
+            if (retornoNfeStatusServico == null) return false;
+
+            if (retornoNfeStatusServico.Retorno.cStat == 108) return true;
+
+            return false;
+        }
+
+        public static bool ParalisadoSemPrevisaoDeRetorno(this RetornoNfeStatusServico retornoNfeStatusServico)
+        {
+            if (retornoNfeStatusServico == null) return false;
+
+            if (retornoNfeStatusServico.Retorno.cStat == 109) return true;
+
+            return false;
+        }
+
+    }
+}
